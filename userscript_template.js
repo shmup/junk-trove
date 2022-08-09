827e03e3dc59d3821bf9df3fae0229f1aeb972bf
Jared Miller <jared@smell.flowers>
2022-08-09 17:56:49 -0400

    Add a boilerplate template for userscripts

---
+++ userscript_template.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         CHANGE ME
+// @namespace    https://codeberg.org/shmup/junk-trove
+// @version      1.0
+// @description  CHANGE ME
+// @author       shmup
+// @match        https://www.example.com/*
+// @match        https://example.com/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+// https://www.tampermonkey.net/documentation.php
+
+(function () {
+  console.log("Check that I run on your specified @match urls")
+})();
